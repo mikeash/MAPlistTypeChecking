eeb6dffb2ca752475915f2cefd31f3c44f7c1842
Michael Ash <mike@mikeash.com>
2013-08-31 17:57:38 -0400

    Add test target

---
+++ MAPlistTypeCheckingTests/MAPlistTypeCheckingTests.h
@@ -0,0 +1,13 @@
+//
+//  MAPlistTypeCheckingTests.h
+//  MAPlistTypeCheckingTests
+//
+//  Created by Michael Ash on 8/31/13.
+//  Copyright (c) 2013 mikeash. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MAPlistTypeCheckingTests : SenTestCase
+
+@end
---
+++ MAPlistTypeCheckingTests/MAPlistTypeCheckingTests.m
@@ -0,0 +1,32 @@
+//
+//  MAPlistTypeCheckingTests.m
+//  MAPlistTypeCheckingTests
+//
+//  Created by Michael Ash on 8/31/13.
+//  Copyright (c) 2013 mikeash. All rights reserved.
+//
+
+#import "MAPlistTypeCheckingTests.h"
+
+@implementation MAPlistTypeCheckingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MAPlistTypeCheckingTests");
+}
+
+@end
